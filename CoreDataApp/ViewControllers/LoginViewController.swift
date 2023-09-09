4a51a8ecf4be6617bb9e21d95de2918d86eb0d00
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-09-08 22:31:12 -0700

    Interface ready

--- CoreDataApp/ViewControllers/LoginViewController.swift
+++ CoreDataApp/ViewControllers/LoginViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonPressed() {
+    }
 }
 
--- CoreDataApp/ViewControllers/RegistrationViewController.swift
+++ CoreDataApp/ViewControllers/RegistrationViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonPressed() {
     }
-    */
-
+    @IBAction func cancelButtonPressed() {
+    }
 }
