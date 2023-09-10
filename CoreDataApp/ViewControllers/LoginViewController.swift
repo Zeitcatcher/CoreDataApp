783d30d764a083355c30ae100367e53956c57f15
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-09-09 21:30:49 -0700

    Basic login logic added

--- CoreDataApp/ViewControllers/LoginViewController.swift
+++ CoreDataApp/ViewControllers/LoginViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private var users: [User] = []
 
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -17,6 +19,32 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginButtonPressed() {
+        guard let login = loginTextField.text, let password = passwordTextField.text else { return }
+
+        fetchDataFromDB(login: login)
+
+        guard let user = users.first else {
+            print("Something is wrong")
+            return
+        }
+        
+        if user.password == password {
+            print("Horray")
+        }
+    }
+}
+
+//MARK: - Private Methods
+extension LoginViewController {
+    private func fetchDataFromDB(login: String) {
+        StorageManager.shared.fetchData(login: login) { [weak self] result in
+            switch result {
+            case .success(let users):
+                self?.users = users
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
